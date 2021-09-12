76f3ec3724e37b601971303a52997d11f5dd3ad8
Skicom123 <fajarrahmanarajab@gmail.com>
2021-09-12 15:44:24 +0700

    Full Game
    -

---
+++ Assets/Scripts/DestroyCoin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCoin : MonoBehaviour
+{
+    public GameObject BotWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        // Jika objek tersebut bernama "Botwall":
+        if (anotherCollider.name == "Botwall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    private AudioSource Tap;
+
+    void Start()
+    {
+        Tap = GetComponent<AudioSource>();
+
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+        Tap.Play();
+
+    }
+}
